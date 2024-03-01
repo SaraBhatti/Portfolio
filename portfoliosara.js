15633272d83e12e7fa293b0e7a221f33c044cf9d
Sara <136837861+SaraBhatti@users.noreply.github.com>
2024-03-01 16:25:29 +0000

    Add files via upload

---
+++ portfoliosara.js
@@ -0,0 +1,9 @@
+// Function to open the popup
+function openPopup() {
+    document.getElementById('popupOverlay').style.display = 'block';
+}
+
+// Function to close the popup
+function closePopup() {
+    document.getElementById('popupOverlay').style.display = 'none';
+}
