eadfb7db3df031b3c7e5bc7311514402ba561a2c
Sara <136837861+SaraBhatti@users.noreply.github.com>
2024-03-14 15:13:53 +0000

    Update portfoliosara.js

--- portfoliosara.js
+++ portfoliosara.js
@@ -1,9 +1,38 @@
-// Function to open the popup
-function openPopup() {
-    document.getElementById('popupOverlay').style.display = 'block';
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Function to open the modal
+function openModal() {
+  modal.style.display = "block";
 }
 
-// Function to close the popup
-function closePopup() {
-    document.getElementById('popupOverlay').style.display = 'none';
+// Function to close the modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// Close the modal if user clicks outside of it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    closeModal();
+  }
+}
+
+
+function predictNews() {
+  var news = document.getElementById("inputNews").value;
+
+  // Make an AJAX request to your Python code 
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://github.com/SaraBhatti/FakeNews/blob/main/FakeNewsBERT.ipynb", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          var response = JSON.parse(xhr.responseText);
+          document.getElementById("predictionResult").innerText = response.prediction;
+      }
+  };
+
+  xhr.send(JSON.stringify({ news: news }));
 }
